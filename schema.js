20a79b220f2692f689ecb9e479f60772602e3f04
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-24 19:47:06 -0300

    library-backend-update

--- schema.js
+++ schema.js
@@ -1,7 +1,37 @@
-const typeDefs = /* GraphQL */`
-    type Author {
-        name: String!
-        id: ID!
-        born: Int
-    }
+const typeDefs = /* GraphQL */ `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book  
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author    
+  }
 `
