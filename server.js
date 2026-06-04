6b9b7172d86c531ff6f52f9e3effc0574f08551d
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-04 19:17:28 -0300

    library-backend-update

--- server.js
+++ server.js
@@ -6,10 +6,24 @@ const resolvers = require('./resolvers')
 const typeDefs = require('./schema')
 const User = require('./models/User')
 
+const getUserFromAuthHeader = async (auth) => {
+  if (!auth || !auth.startsWith('Bearer ')) {
+    return null
+  }
+ 
+  const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+  return User.findById(decodedToken.id).populate('friends')
+}
+
 const startServer = (port) => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: async ({ req }) => {
+      const auth = req.headers.authorization || ''
+      const user = await getUserFromAuthHeader(auth)
+      return { user }
+    }
   })
 
   startStandaloneServer(server, {
