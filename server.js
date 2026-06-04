ca141313e4fb0326dfd76bb3e8d5e4ffc460d047
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-04 19:30:05 -0300

    library-backend-update

--- server.js
+++ server.js
@@ -10,24 +10,27 @@ const getUserFromAuthHeader = async (auth) => {
   if (!auth || !auth.startsWith('Bearer ')) {
     return null
   }
- 
-  const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
-  return User.findById(decodedToken.id)
+  try {
+    const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+    return User.findById(decodedToken.id)
+  } catch {
+    return null
+  }
 }
 
 const startServer = (port) => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: async ({ req }) => {
-      const auth = req.headers.authorization
-      const currentUser = await getUserFromAuthHeader(auth)
-      return { user: currentUser }
-    }
   })
 
   startStandaloneServer(server, {
     listen: { port },
+    context: async ({ req }) => {
+      const auth = req.headers.authorization
+      const currentUser = await getUserFromAuthHeader(auth)
+      return { currentUser }
+    }
   }).then(({ url }) => {
     console.log(`Server ready at ${url}`)
   })
