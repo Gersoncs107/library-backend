537c3945dd2c74da5e6d5e1cf0b36ba58beeb6b5
Gersoncs107 <gerson.silva107@gmail.com>
2026-07-12 22:43:51 -0300

    library-backend-update

--- resolvers.js
+++ resolvers.js
@@ -61,7 +61,9 @@ const resolvers = {
     const authors = await Author.find({})
 
     return authors.map(author => ({
-      ...author.toObject(),
+      id: author._id.toString(),   // ← id explícito
+      name: author.name,
+      born: author.born,
       bookCount: countMap[author._id.toString()] || 0
     }))
   },
